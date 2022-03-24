6d19199655d305d5363751de77e30ff5f63b4622
elias puebla <elipuebla01@gmail.com>
2022-03-24 19:09:48 -0300

    sin cambios

--- AppCoder1/views.py
+++ AppCoder1/views.py
@@ -1,14 +1,5 @@
-from contextlib import ContextDecorator
-from contextvars import Context
-from ctypes.wintypes import tagMSG
-from curses.ascii import HT
-from pipes import Template
-from re import template
-from tempfile import tempdir
-from urllib import request
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.template import Context, Template
 # Create your views here.
 
 def inicio(request):
--- ProyectoCoder1/urls.py
+++ ProyectoCoder1/urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 # from .views import otrodiccionario, primertemplate, saludo
 
 
 
 urlpatterns = [path('admin/', admin.site.urls),
-               path('AppCoder1/',include("AppCoder1.urls"))]
+               path('AppCoder1/',include('AppCoder1.urls')),
+               ]
 
